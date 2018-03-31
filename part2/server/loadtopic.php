8ec152a8fa2f13286a2a981ec770d71d65d9221b
enzozafra <zafra@ualberta.ca>
2018-03-30 22:32:33 -0600

    replaced data EML style

--- part2/server/loadtopic.php
+++ part2/server/loadtopic.php
@@ -12,10 +12,7 @@ else {
 	if ($unitid) {
 		$topics = DB::run("SELECT * FROM topics WHERE units_unitid=?", [$unitid]);
 		while ($topic = $topics->fetch(PDO::FETCH_LAZY)) {
-			/* TODO: change <> to {} or some sort of delimiter for EML */
-echo '<script>
-    console.log(json_encode('.addslashes($topic['content']).'); ?>);
-</script>';
+			var_dump($topic);
 		}
 	}
 }
