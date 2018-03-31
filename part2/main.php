4b445632a767fc73568784c3dc8d48bf7ecc4eb8
enzozafra <zafra@ualberta.ca>
2018-03-30 19:54:01 -0600

    shows units for course im registered in

--- part2/main.php
+++ part2/main.php
@@ -13,36 +13,33 @@ if (!isset($_SESSION['loggeduser'])) {
 			<h4>Course Overview</h4>
 			<hr>
 			<ul class="collapsible">
-				<li>
-					<div class="collapsible-header">COMP 466</div>
-					<div class="collapsible-body">
-						<ul class="browser-default">
-							<li><a>Unit 1</a></li>
-							<li><a>Unit 2</a></li>
-							<li><a>Unit 3</a></li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<div class="collapsible-header">COMP 466</div>
-					<div class="collapsible-body">
-						<ul class="browser-default">
-							<li><a>Unit 1</a></li>
-							<li><a>Unit 2</a></li>
-							<li><a>Unit 3</a></li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<div class="collapsible-header">COMP 466</div>
-					<div class="collapsible-body">
-						<ul class="browser-default">
-							<li><a>Unit 1</a></li>
-							<li><a>Unit 2</a></li>
-							<li><a>Unit 3</a></li>
-						</ul>
-					</div>
-				</li>
+				<?php
+				include 'helper/database.php';
+				$id = $_SESSION['loggeduser'];
+				if ($id) {
+					$mycourses = DB::run('SELECT * from courses where courseid in (select courses_courseid from users_has_courses where users_userid=?);', [$id]);
+					$rows = $mycourses->fetchAll(PDO::FETCH_ASSOC);
+					if($rows) {
+						foreach ($rows as $row)
+						{
+							echo '<li>';
+							echo '<div class="collapsible-header">' . $row['coursename'] . '</div>';
+							$units = DB::run('SELECT * from units where courses_courseid=?', [$row['courseid']]);
+							while ($unit = $units->fetch(PDO::FETCH_LAZY))
+							{
+							echo '<div class="collapsible-body"> <a href="TODO">' . $unit['unitname'] . '</a> </div>';
+							}
+							echo '</li>';
+						}
+					}
+					else {
+						echo '<b>You are not registered for any courses. To register, please visit the <a href="courses.php">Register</a> page</b>';
+					}
+				}
+				else {
+					header('Location: notlogged.php');
+				}
+				?>
 			</ul>
 		</div>
 	</div>
